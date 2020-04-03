584bc7dfb633e2f5bfaaa47dae54c2ad0c1b2f70
Sylvain Brisebois <sbrisebois@kinova.ca>
2020-04-02 22:47:40 -0400

    Update pump from pwm to servo control

--- tlc/control.cpp
+++ tlc/control.cpp
@@ -12,15 +12,14 @@
 #include "lcd_keypad.h"
 
 ServoTimer2 exhaleValveServo;
+ServoTimer2 pumpServo;
 
 bool Control_Init()
 {
     gDataModel.nTickRespiration = millis(); // Respiration cycle start tick. Used to compute respiration per minutes
     exhaleValveServo.write(gConfiguration.nServoExhaleOpenAngle);
-
-    Timer1.initialize(4000);         // initialize timer1, and set a 4000us period
-    Timer1.pwm(PIN_OUT_PUMP1_PWM, 0);                // setup pwm on pin 9, 50% duty cycle  ( 0 to 1000)
-
+    pumpServo.write(750);
+    
     gDataModel.bStartFlag = false;
 
     return true;
@@ -294,7 +293,7 @@ void Control_Process()
         gDataModel.nCycleState = kCycleState_WaitTrigger;
         exhaleValveServo.write(gConfiguration.nServoExhaleOpenAngle);
         gDataModel.nTickRespiration = millis(); // Respiration cycle start tick. Used to compute
-        Timer1.pwm(PIN_OUT_PUMP1_PWM, gDataModel.nPWMPump);
+        pumpServo.write((gDataModel.nPWMPump * 1.5) + 750);
         return;
     }
 
@@ -321,5 +320,5 @@ void Control_Process()
     };
 
     // Pump power to output
-    Timer1.pwm(PIN_OUT_PUMP1_PWM, gDataModel.nPWMPump);
+    pumpServo.write((gDataModel.nPWMPump * 1.5) + 750);
 }
